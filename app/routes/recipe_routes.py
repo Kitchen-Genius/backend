518702195c97c75e2c6d7c105f5b52b9208de45a
EstyBrandes <estystbrandes@gmail.com>
2024-03-01 23:33:39 +0200

    receipe routes

--- app/routes/recipe_routes.py
+++ app/routes/recipe_routes.py
@@ -3,6 +3,30 @@ from app.services import spoonacular as spoonacular_service
 from app.services.recipe_processing import process_and_save_recipes, prepare_recipe_search_criteria
 from app.utils.validations import validate_diet, validate_type, validate_intolerances
 from app.models.recipe_models import ProcessRecipesCriteria
+from pymongo import MongoClient
+
+MONGODB_URI = "mongodb+srv://KGUser:jXH2M8loFrZjtSYR@cluster0.v1oaihv.mongodb.net/"
+DATABASE_NAME = "KitchenGenius"
+COLLECTION_NAME = "saved_recipies"
+# Provide the connection details
+hostname = 'cluster0.v1oaihv.mongodb.net'
+port = 27017  # Default MongoDB port
+username = 'KGUser'  # If authentication is required
+password = 'jXH2M8loFrZjtSYR'  # If authentication is required
+
+# Create a MongoClient instance
+
+client = MongoClient(MONGODB_URI )
+print("#####################")
+print(client)
+
+db = client[DATABASE_NAME]
+collection = db[COLLECTION_NAME]
+
+class Item:
+    def __init__(self, name: str, description: str):
+        self.name = name
+        self.description = description
 
 router = APIRouter()
 
@@ -82,3 +106,31 @@ async def process_recipes_criteria(criteria: ProcessRecipesCriteria = Body(...))
         return processed_recipes_list
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
+    
+
+
+
+@router.post("/atlas/save_recipe")
+async def save_recipe(recipe: dict = Body(...)):
+    try:
+        # collection.insert_one(
+        #     {"User": recipe["user"]},
+        #     {"$set": {"recipeCollectio": recipe }}
+        # )
+        result = collection.insert_one(recipe)
+        return {"message": "Recipe saved successfully", "id": str(result.inserted_id)}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+    
+
+@router.get("/atlas/get_recipes")
+async def get_recipes():
+    try:
+        recipes = collection.find()
+        return recipes
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+
+
+
