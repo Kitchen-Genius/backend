d2ab6435a423a6bc8fa7b65479590f9289cac4de
Reshaflame“ <reshaflame@gmail.com>
2024-03-06 21:33:46 +0200

    mispell

--- app/services/auth_service.py
+++ app/services/auth_service.py
@@ -20,5 +20,5 @@ async def authenticate_user(email: str, password: str):
         return None
 
 async def get_user_by_email(email: str):
-    user_doc = await saved_recipies.find_one({"email": email})
+    user_doc = await users.find_one({"email": email})
     return user_doc  # You might want to serialize this document before returning
