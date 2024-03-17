94198913ee7b9b65e60efcb1b07a160cc67b98c9
Reshaflame“ <reshaflame@gmail.com>
2024-03-17 15:34:54 +0200

    last step to fetch favorites 5/5

--- app/routes/user_routes.py
+++ app/routes/user_routes.py
@@ -1,6 +1,7 @@
-from fastapi import APIRouter, HTTPException
+from fastapi import APIRouter, Body, HTTPException
 from app.models.request_models import FavoriteRecipeRequest
 from app.database.user_utils import update_user_favorites  
+from app.services.recipe_management import fetch_user_favorite_recipes
 
 router = APIRouter()
 
@@ -12,5 +13,13 @@ async def update_favorites(request: FavoriteRecipeRequest):
             return {"message": "User's favorites updated successfully"}
         else:
             return {"message": "User or recipe not found"}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+@router.post("/users/favorites/list")
+async def fetch_user_favorites(user_id: int = Body(..., embed=True)):
+    try:
+        favorites = await fetch_user_favorite_recipes(user_id)
+        return {"favorites": favorites}
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
